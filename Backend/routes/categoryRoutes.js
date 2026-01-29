753b561bbf3d0a621165435fee0ae7c35e06186c
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2026-01-29 11:53:40 +0430

    new

--- Backend/routes/categoryRoutes.js
+++ Backend/routes/categoryRoutes.js
@@ -60,12 +60,7 @@ router.delete("/:id", authMiddleware, (req, res) => {
 });
 
 
-import { createCategory, createSubCategory, getAllCategories } from "../controllers/categoryController.js";
 
-const router = express.Router();
-router.post("/", createCategory);      // ساخت دسته
-router.post("/sub", createSubCategory); // ساخت زیرمجموعه
-router.get("/", getAllCategories);     // دریافت همه
 
 
 
